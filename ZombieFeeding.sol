0e4bd72a126aa963ecc98d062968aea5ade3e0d2
nicolasODT <nicolas.terwagne@gmail.com>
2023-12-24 14:42:56 +0100

    a

--- ZombieFeeding.sol
+++ ZombieFeeding.sol
@@ -4,4 +4,13 @@ pragma solidity ^0.8.19;
 
 import "./zombiefactory.sol";
 
-contract ZombieFeeding is ZombieFactory {}
+
+contract ZombieFeeding is ZombieFactory {
+    function feedAndMultiply(uint _zombieId, uint _targetDna) public {
+        require(msg.sender == zombieToOwner[_zombieId]);
+        Zombie storage myZombie = zombies[_zombieId];
+        _targetDna = _targetDna % dnaModulus;
+        uint newDna = (myZombie.dna + _targetDna) / 2;
+        _createZombie("NoName", newDna);
+    }
+}
